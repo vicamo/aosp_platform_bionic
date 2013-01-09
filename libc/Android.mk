bff0d902f91d7e0dcc80e04f426a0952e6fc8ce0
Rom Lemarchand <romlem@google.com>
2013-01-09 15:46:06 -0800

    Add signalfd call to bionic
    Add signalfd() call to bionic.
    
    Adding the signalfd call was done in 3 steps:
    - add signalfd4 system call (function name and syscall
      number) to libc/SYSCALLS.TXT
    - generate all necessary headers by calling
      libc/tools/gensyscalls.py. This patch is adding
      the generated files since the build system
      does not call gensyscalls.py.
    - create the signalfd wrapper in signalfd.cpp and add
      the function prototype to sys/signalfd.h
    
    (cherry-pick of 0c11611c11f4dc1b6d43587b72c3ccbe8c51a51c, modified to
    work with older versions of GCC still in use on some branches.)
    
    Change-Id: I4c6c3f12199559af8be63f93a5336851b7e63355

--- libc/Android.mk
+++ libc/Android.mk
@@ -278,6 +278,7 @@ libc_common_src_files := \
 	bionic/sched_cpucount.c \
 	bionic/semaphore.c \
 	bionic/sha1.c \
+	bionic/signalfd.cpp \
 	bionic/ssp.c \
 	bionic/stubs.c \
 	bionic/system_properties.c \
